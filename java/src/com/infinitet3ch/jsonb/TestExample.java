ff6304b0ce1f034a4b07d3543b2d61dd6ef58e82
Nadir Hamid <contact@infinitet3ch.com>
2018-11-08 22:59:02 -0700

    update padding on java files

--- java/src/com/infinitet3ch/jsonb/TestExample.java
+++ java/src/com/infinitet3ch/jsonb/TestExample.java
@@ -20,55 +20,54 @@ class TestExample {
     public static void dumpObjectIndex(Dog obj, int index) {
         TestExample.dump( obj, "index(" + index + ")" );
     }
-	public static void main(String[] args) {
-		// Obtain the class object if we know the name of the class
-		Class exampleClass = Dog.class;
-		try {
-			int i;
-            int j;
-			String str = "";
+  public static void main(String[] args) {
+    // Obtain the class object if we know the name of the class
+    Class exampleClass = Dog.class;
+    try {
+      int i;
+      int j;
+      String str = "";
       System.out.println("starting JSON-B demonstration..");
-			Reflection reflection = new Reflection();
-            String parseObjStr = "{\"name\": \"Falco\", \"age\": 4, \"bitable\": false}";
-			String parseObjArrStr = "[{\"name\": \"Falco\", \"age\": 4, \"bitable\": false}, {\"name\": \"Cassidy\", \"age\": 5, \"bitable\": false}]";
-            String parsedObjStr = "";
-            String parsedObjArrStr = "";
-            Dog[] objs;
-            Dog obj;
-            Dog[] reparsedObjs;
-            Dog reparsedObj;
+      Reflection reflection = new Reflection();
+      String parseObjStr = "{\"name\": \"Falco\", \"age\": 4, \"bitable\": false}";
+      String parseObjArrStr = "[{\"name\": \"Falco\", \"age\": 4, \"bitable\": false}, {\"name\": \"Cassidy\", \"age\": 5, \"bitable\": false}]";
+      String parsedObjStr = "";
+      String parsedObjArrStr = "";
+      Dog[] objs;
+      Dog obj;
+      Dog[] reparsedObjs;
+      Dog reparsedObj;
 
-			for ( i = 0; i != 10; i ++ ) {
-                System.out.println("test trial (" + i + ")");
-                obj = (Dog) reflection.fromJSONB(parseObjStr, exampleClass);
-                TestExample.dumpObject( obj );
-                objs = (Dog[]) reflection.fromJSONBArray(parseObjArrStr, exampleClass);
-                for (j = 0; j != objs.length; j ++ ) {
-                    obj= objs[ j ];
-                    TestExample.dumpObjectIndex( obj, j );
-                }
-                for ( j= 0; j != 10; j ++ ) {
-                    parsedObjStr = reflection.toJSONB(objs[0]);
-                    System.out.println("toJSONB result = " + parsedObjStr);
-                    parsedObjArrStr = reflection.toJSONBArray(objs, exampleClass);
-                    System.out.println("toJSONBArray result = " + parsedObjArrStr);
-                }
-                reparsedObj = (Dog) reflection.fromJSONB(parsedObjStr, exampleClass);
-			    TestExample.dumpObject( reparsedObj );
-                reparsedObjs = (Dog[]) reflection.fromJSONBArray(parsedObjArrStr, exampleClass);
-			    for ( j = 0; j != reparsedObjs.length; j ++ ) {
-                    obj = reparsedObjs[ j ];
-                    TestExample.dumpObjectIndex( obj, j );
-               }
-            }
-        } catch (ParserException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+      for ( i = 0; i != 10000000; i ++ ) {
+        System.out.println("test trial (" + i + ")");
+        obj = (Dog) reflection.fromJSONB(parseObjStr, exampleClass);
+        TestExample.dumpObject( obj );
+        objs = (Dog[]) reflection.fromJSONBArray(parseObjArrStr, exampleClass);
+        for (j = 0; j != objs.length; j ++ ) {
+          obj= objs[ j ];
+          TestExample.dumpObjectIndex( obj, j );
+        }
+        for ( j= 0; j != 10; j ++ ) {
+          parsedObjStr = reflection.toJSONB(objs[0]);
+          System.out.println("toJSONB result = " + parsedObjStr);
+          parsedObjArrStr = reflection.toJSONBArray(objs, exampleClass);
+          System.out.println("toJSONBArray result = " + parsedObjArrStr);
+        }
+        reparsedObj = (Dog) reflection.fromJSONB(parsedObjStr, exampleClass);
+        TestExample.dumpObject( reparsedObj );
+        reparsedObjs = (Dog[]) reflection.fromJSONBArray(parsedObjArrStr, exampleClass);
+        for ( j = 0; j != reparsedObjs.length; j ++ ) {
+          obj = reparsedObjs[ j ];
+          TestExample.dumpObjectIndex( obj, j );
+        }
+      }
+    } catch (ParserException e) {
+      e.printStackTrace();
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-	}
-
+  }
 }
 
